ab991981a28738c957aceb999a65e5e86039fa74
michael <michaeljohn.cgatab@gmail.com>
2023-04-08 09:22:52 +0800

    Implement strong type state and selector in User

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,17 +1,31 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
+import { User } from "../user";
 
 export interface UserState {
-    maskUserName: boolean
+    maskUserName: boolean,
+    currentUser: User,
 }
 
-export const userReducer = createReducer(
-    {
-        maskUserName: false
-    },
-    on(createAction('[User] Toggle Mask Username'), state => {
+const initialState = {
+    maskUserName: false,
+    currentUser: null
+}
+
+export const userReducer = createReducer<UserState>(
+    initialState,
+    on(createAction('[User] Toggle Mask Username'), (state):UserState => {
         return {
             ...state,
             maskUserName: !state.maskUserName
         }
     })
 );
+
+//SELECTOR
+
+const getUserFeatureState = createFeatureSelector<UserState>('users');
+
+export const getMaskUserName = createSelector(
+    getUserFeatureState,
+    state => state.maskUserName
+)
